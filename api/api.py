f48ccc0df63199bf7f9ddb8f5ffacf18bcc961ce
nik mac <naldorsc2@gmail.com>
2024-11-20 07:43:40 +0100

    commit

--- api/api.py
+++ api/api.py
@@ -3,9 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route('/time')
 def get_current_time():
     # return{'time': "test"}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+export default function Header() {
+    return (
+    <Box sx={{m: 0, p: 5,bgcolor: 'red', width: '100vh'}}>
+      <p>Test</p>
+    </Box>
+    )
+}
