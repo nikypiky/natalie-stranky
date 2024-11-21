68b06c4845c89104380e4d5df63895a832c1bbb3
nik mac <naldorsc2@gmail.com>
2024-11-21 17:32:37 +0100

    fixed padding issue with header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import Header from './components/Header'
-import Container from "@mui/material/Container"
+	import './App.css';
+	import Container from "@mui/material/Container"
+import Header from './components/Header';
+	// import { Box } from '@mui/material';
 
 
-function App() {
-	return (
-		<>
-			<Container sx={{bgcolor: 'blue'}}display="flex" maxWidth={false} disableGutters>
-				<Header />
-				<p>test</p>
-			</Container>
-		</>
-	)
-}
+	function App() {
+		return (
+			<>
+				<Container sx={{ bgcolor: 'blue' }} display="flex" maxWidth={false} disableGutters>
+					{/* <div id='header'> */}
+						{/* <p>Test</p> */}
+						<Header />
+					{/* </div> */}
+					<p>test</p>
+				</Container>
+			</>
+		)
+	}
 
-export default App;
+	export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Box } from "@mui/material"
 
 export default function Header() {
     return (
-    <Box sx={{m: 0, p: 5,bgcolor: 'red', width: '100vh'}}>
+    <Box sx={{m: 0, p: 5,bgcolor: 'red', width: '100%'}}>
       <p>Test</p>
     </Box>
     )
