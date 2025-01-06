cf9c823463a11053e7b7a4ab424af34268444f2e
Your Name <naldorsc2@gmail.com>
2025-01-06 20:45:13 +0100

    password and username now saved to variable

--- src/components/adminPage/Login.js
+++ src/components/adminPage/Login.js
@@ -1,14 +1,27 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
+// import { useField } from "@mui/x-date-pickers/internals";
+import { useState } from 'react';
 
 export default function Login() {
 
 	// define state values for login input
-	const [loginValue, setLoginValue] = useState({
-		login: "",
+	const [login, setLogin] = useState({
+		username: "",
 		password: "",
-	});
+	})
 
-	onsubmi
+	const handleOnChange = (event) => {
+		const { name, value } = event.target;
+		setLogin({
+			...login,
+			[name]: value,
+		})
+	};
+
+	const handleSubmit = (ev) => {
+		ev.preventDefault();
+		alert('Your username is: ' + login.username + login.password)
+	};
 
 	return (
 		<>
@@ -16,15 +29,20 @@ export default function Login() {
 				sx={{
 					display: 'flex',
 					flexDirection: 'column',
-					width: '500px',
 					alignItems: 'center',
 					gap: 2, // Adds space between the elements
 				}}
 				component="form"
+				onSubmit={handleSubmit}
+				onChange={handleOnChange}
 			>
 				<p style={{ textAlign: 'center', marginBottom: 20 }}>Login</p>
-				<TextField name="username" label='Username' fullWidth />
-				<TextField name="password" label='Password' fullWidth />
+				<TextField name="username" label='Username' onChange={handleOnChange}/>
+				<TextField name="password" label='Password' onChange={handleOnChange}/>
+				<Button variant="contained" type='submit'>Submit</Button>
 			</Box>
+			{/* <form onSubmit={onSubmit}>
+				<input type="text" ref={usernameField} />
+			</form> */}
 		</>);
 }
