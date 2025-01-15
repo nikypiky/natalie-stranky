10adac10c8f50829227551c9a6ef21892694b04f
Your Name <naldorsc2@gmail.com>
2025-01-15 22:59:33 +0100

    date imput got too

--- src/components/adminPage/AddFreeDates.js
+++ src/components/adminPage/AddFreeDates.js
@@ -2,62 +2,64 @@ import { TimePicker, DatePicker, LocalizationProvider } from "@mui/x-date-picker
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Box } from "@mui/material";
 import { useState } from 'react';
-import dayjs from "dayjs";
+// import dayjs from "dayjs";
+
 
 export default function AddFreeDates() {
 
 	// const [freeDate, setFreeDate] = useState(dayjs())
 	const [freeDate, setFreeDate] = useState({
-		date: dayjs(),
-		start: dayjs(),
-		end: dayjs(),
+		date: null,
+		start: "",
+		end: "",
 	})
 
 	if (freeDate) {
 		console.log("start: ", freeDate.start)
 		console.log("end: ", freeDate.end)
+		console.log("date: ", freeDate.date)
 	}
 
 	const onDateChange = (key, newValue) => {
-		setFreeDate(
+		setFreeDate((freeDate) => ({
 			...freeDate,
-			freeDate[key] = newValue
-		)
+			[key]: newValue.format("DD-MM-YYYY")
+		}))
 	}
 
 	const onTimeChange = (key, newValue) => {
 		setFreeDate((freeDate) => ({
 			...freeDate,
 			[key]: newValue.format("HH:mm")
-	}))
-}
+		}))
+	}
 
-return (
-	<>
-		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<Box
-				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					gap: 2, // Adds space between the elements
-				}}
-			>
-				<p style={{ textAlign: 'center', marginBottom: 20 }}>Login</p>
-				{/* <DatePicker
-						value={freeDate.date}
-						onChange={(newValue) => console.log(newValue)}/> */}
-				<TimePicker
-					ampm={false}
-					minutesStep={15}
-					// value={freeDate.start}
-					onChange={(newValue) => onTimeChange("start", newValue)} />
-				<TimePicker
-					ampm={false}
-					minutesStep={15}
-					// value={freeDate.end}
-					onChange={(newValue) => onTimeChange("end", newValue)} />
-			</Box>
-		</LocalizationProvider>
-	</>);
+	return (
+		<>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						gap: 2, // Adds space between the elements
+					}}
+				>
+					<p style={{ textAlign: 'center', marginBottom: 20 }}>Login</p>
+					<DatePicker
+						// value={freeDate.date}
+						onChange={(newValue) => onDateChange("date", newValue)} />
+					<TimePicker
+						ampm={false}
+						minutesStep={15}
+						// value={freeDate.start}
+						onChange={(newValue) => onTimeChange("start", newValue)} />
+					<TimePicker
+						ampm={false}
+						minutesStep={15}
+						// value={freeDate.end}
+						onChange={(newValue) => onTimeChange("end", newValue)} />
+				</Box>
+			</LocalizationProvider>
+		</>);
 }
